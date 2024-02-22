d6cc4ce5c297155a03b19eb09e13fcfca9513fae
justinschuster <schujustin@gmail.com>
2024-02-22 00:26:12 -0500

    More writer stuff

--- pkg/ansi/ansi.go
+++ pkg/ansi/ansi.go
@@ -6,6 +6,14 @@ import (
   "io"
 )
 
+type Writer struct {
+  writer io.Writer
+}
+
 func NewWriter(w io.Writer) *Write {
   return &Writer{writer: w}
+}
+
+func (w *Writer) Write(data []byte) (n int, err error) {
+  return w.writer.Write(data)
 }
