5c1855b78994a711c08889d6121d06500e5d0158
justinschuster <schujustin@gmail.com>
2024-02-27 09:24:57 -0500

    Remove comments

--- pkg/multiplexer/multiplexer.go
+++ pkg/multiplexer/multiplexer.go
@@ -2,39 +2,7 @@ package multiplexer
 
 import (
   "fmt"
-  // "os"
-  // "io"
-
-  // "github.com/justinschuster/gomux/pkg/ansi" 
-  // "github.com/justinschuster/gomux/pkg/pane"
 )
-
-// type Multiplexer struct {
-  // activePane    pane.Pane
-  // closeChan     chan struct{}
-  // ouput         chan byte
-  // updateChan    <-chan pane.Pane
-// }
-
-// func New() *Multiplexer {
-  // update := make(chan pane.Pane, 0xff)
-  // out := make(chan byte, 0xffff)
-  // stdoutWriter := NewChanWriter(out) 
-  // terminalPane := pane.newTerminalPane(update)
-
-  // mp := &Multiplexer{
-    // activePane: terminalPane,
-    // output: out,
-    // closeChan:  make(chan struct{}),
-  // }
-  // return mp
-// }
-
-// func (m *Multiplexer) Start() error {
-  // fmt.Println("Hello")
-  // _, err = io.Copy(os.Stdout, m)
-  // return err
-// }
 
 type Multiplexer struct {
   outputChan chan byte
