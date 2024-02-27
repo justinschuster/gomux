b902a1a053cf325403ae08e8a49bfa3c18f55c7b
justinschuster <schujustin@gmail.com>
2024-02-27 09:40:57 -0500

    more formatting

--- pkg/multiplexer/multiplexer.go
+++ pkg/multiplexer/multiplexer.go
@@ -1,14 +1,10 @@
 package multiplexer
 
-import (
-  "fmt"
-)
-
 type Multiplexer struct {
   num int
 }
 
 func newMultiplexer(age int) *Multiplexer {
   mp := &Multiplexer{num: age}
-  return &mp
+  return mp
 }
